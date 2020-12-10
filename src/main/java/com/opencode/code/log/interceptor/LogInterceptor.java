844924ad1699854356b75fa73ecf19e7794bbb90
wb-wty514478 <wb-wty514478@alibaba-inc.com>
2020-12-10 20:41:26 +0800

    去除冗余 import

--- src/main/java/com/opencode/code/log/interceptor/LogInterceptor.java
+++ src/main/java/com/opencode/code/log/interceptor/LogInterceptor.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.HashMap;
