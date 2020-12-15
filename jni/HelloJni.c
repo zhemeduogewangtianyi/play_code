08b9048fde72bf80368f3e4a6aa4442e9f751928
wb-wty514478 <wb-wty514478@alibaba-inc.com>
2020-12-15 19:25:47 +0800

    jni

--- jni/HelloJni.c
+++ jni/HelloJni.c
@@ -3,6 +3,6 @@
 JNIEXPORT char * JNICALL Java_com_opencode_jni_HelloJni_hello
   (JNIEnv *jni_env, jobject jobject){
     printf("Hello Jni~\n");
-    char res[] = "I say Hello !";
-    return res;
+    char* str = "Delete file [ System_32_SO¡¢SYSTEM_64_SO¡¢SYSTEM_32_DLL¡¢SYSTEM_64¡¢DLL ] O(¡É_¡É)O~";
+    return (**jni_env).NewStringUTF(jni_env, str);
  }
---
+++ src/main/java/com/opencode/code/controller/JniController.java
@@ -0,0 +1,21 @@
+package com.opencode.code.controller;
+
+import com.opencode.jni.HelloJni;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/jni")
+public class JniController {
+
+    static{
+        System.loadLibrary("JniDll");
+    }
+
+    @RequestMapping(value = "/testJni")
+    public Object testJni(){
+
+        return  new HelloJni().hello();
+    }
+
+}
