e4460595a620f993c6cee7bbe634c10b39dbd715
WTY <wtywty521@gmail.com>
2021-05-05 10:26:40 +0800

    脚本DO

---
+++ src/main/java/com/opencode/code/listeners/change2/entity/daoObject/ScriptDO.java
@@ -0,0 +1,20 @@
+package com.opencode.code.listeners.change2.entity.daoObject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScriptDO {
+
+    private Long id;
+
+    private String name;
+
+    private Integer type;
+
+    private Integer status;
+
+    private String script;
+
+}
