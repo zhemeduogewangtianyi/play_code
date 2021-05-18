18d383bdd861f79b74b8eb3da109610145627eb6
wb-wty514478 <wb-wty514478@alibaba-inc.com>
2021-05-18 17:43:13 +0800

    加密

--- src/main/java/com/opencode/code/password/CarrotPassword.java
+++ src/main/java/com/opencode/code/password/CarrotPassword.java
@@ -1,9 +1,49 @@
 package com.opencode.code.password;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import java.io.UnsupportedEncodingException;
+
 public class CarrotPassword {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 
+        charsPassword();
+
+        System.out.println();
+        System.out.println();
+
+        bytesPassword();
+
+        String data = "{&quot;code&quot;:1}";
+        JSONObject jsonObject = JSONObject.parseObject(StringEscapeUtils.unescapeHtml4(data));
+        System.out.println(jsonObject);
+
+    }
+
+    private static void bytesPassword() throws UnsupportedEncodingException {
+        int slat = 686;
+        String ok = "这是一个非常牛逼的实验！";
+        byte[] bytes = ok.getBytes("UTF-8");
+
+        System.out.println(new String(bytes,"UTF-8"));
+
+        for(int i = 0 ; i < bytes.length ; i++ ){
+            bytes[i] ^= slat;
+        }
+
+        System.out.println(new String(bytes,"UTF-8"));
+
+        for(int i = 0 ; i < bytes.length ; i++ ){
+            bytes[i] ^= slat;
+        }
+
+        System.out.println(new String(bytes,"UTF-8"));
+
+    }
+
+    public static void charsPassword(){
         int slat = 1000000000;
 
         String ok = "这是一个非常牛逼的实验！";
@@ -22,7 +62,6 @@ public class CarrotPassword {
         }
 
         System.out.println(chars);
-
     }
 
 }
