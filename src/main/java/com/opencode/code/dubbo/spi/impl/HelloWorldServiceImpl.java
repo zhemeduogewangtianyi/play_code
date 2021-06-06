d3945400c3c8bfbcc3d345a2c90ae111f5250fc5
WTY <wtywty521@gmail.com>
2021-06-06 13:57:10 +0800

    Dubbo SPI

--- src/main/java/com/opencode/code/dubbo/spi/impl/HelloWorldServiceImpl.java
+++ src/main/java/com/opencode/code/dubbo/spi/impl/HelloWorldServiceImpl.java
@@ -6,7 +6,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public void helloWorld() {
-        System.out.println("Hello World !");
+        System.out.println("Hello Dubbo SPI !");
     }
 
 }
