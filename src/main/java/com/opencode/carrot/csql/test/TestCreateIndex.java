410dff34ebe0bbf24449e5f97e2c07f05c7a9c07
WTY <wtywty521@gmail.com>
2021-06-22 23:03:43 +0800

    数据库雏形

--- src/main/java/com/opencode/carrot/csql/test/TestCreateIndex.java
+++ src/main/java/com/opencode/carrot/csql/test/TestCreateIndex.java
@@ -4,10 +4,7 @@ import com.opencode.carrot.csql.entity.User;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
@@ -19,6 +16,8 @@ import org.apache.lucene.util.NumericUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Date;
+import java.util.List;
 
 /**
 
@@ -51,17 +50,24 @@ import java.nio.file.Path;
  * */
 public class TestCreateIndex {
 
+//    private static final String path = "/Users/wty/Downloads/temp";
+    private static final String path = "D:\\del";
+
+
     public static void main(String[] args) throws IOException, ParseException {
 
         //把数据填充到JavaBean对象中
         User user = new User();
         user.setId(2L);
-        user.setName("week");
+        user.setName("周童童");
         user.setAge(17);
+        user.setDesc("周童童 是一个好学生，太好了，真的是太好了！￥%……");
+        user.setUrl("http://www.baidu.com");
+        user.setBirthDay(new Date());
 
         Analyzer analyzer = new StandardAnalyzer();
 
-        File file = new File("/Users/wty/Downloads/temp");
+        File file = new File(path);
         if(!file.exists()){
             file.mkdir();
         }
@@ -74,9 +80,38 @@ public class TestCreateIndex {
         IndexWriter iWriter = new IndexWriter(directory,config);
 
         Document doc = new Document();
-        doc.add(new NumericDocValuesField("id",user.getId()));
-        doc.add(new Field("name",user.getName(), TextField.TYPE_STORED));
-        doc.add(new NumericDocValuesField("age", user.getAge()));
+
+        //建立 Long 索引
+        LongPoint id_idx = new LongPoint("id", user.getId());
+        doc.add(id_idx);
+
+        //存储ID
+        Field id = new StoredField("id", user.getId());
+        doc.add(id);
+
+        //设置一个文本类型，对内容不分词，并存在文档中储存
+        Field name = new StringField("name", user.getName(), Field.Store.YES);
+        doc.add(name);
+
+        //设置一个文本类型，对内容分词
+        Field desc = new TextField("desc", user.getDesc(), Field.Store.YES);
+        doc.add(desc);
+
+        //建立一个 Int 类型
+        Field age_idx = new IntPoint("age", user.getAge());
+        doc.add(age_idx);
+
+        //存储
+        Field age = new StoredField("age", user.getAge());
+        doc.add(age);
+
+        //建立一个文本类型，不分词，不建立索引，在文档中存储
+        StoredField url = new StoredField("url", user.getUrl());
+        doc.add(url);
+
+        //建立一个日期类型 - 用 long 表示
+        doc.add(new LongPoint("birthDay",user.getBirthDay().getTime()));
+        doc.add(new StoredField("birthDay",user.getBirthDay().getTime()));
 
         iWriter.addDocument(doc);
 
@@ -89,18 +124,19 @@ public class TestCreateIndex {
 //        Query query = parser.parse("1");
 //        TopDocs search = iSearcher.search(query, 10);
 
-        Query id = LongPoint.newExactQuery("id", 2);
-        TopDocs search = iSearcher.search(id, 10);
+        Query queryById = LongPoint.newExactQuery("id", 2);
+        TopDocs search = iSearcher.search(queryById, 10);
 
         for(ScoreDoc hit : search.scoreDocs){
             Document hitDoc = iSearcher.doc(hit.doc);
-            System.out.println(hitDoc.get("id"));
-            System.out.println(hitDoc.get("name"));
-            System.out.println(hitDoc.get("age"));
+            List<IndexableField> fields = hitDoc.getFields();
+            for(IndexableField idxField : fields){
+                System.out.println(idxField.name() + " : " + idxField.getCharSequenceValue());
+            }
         }
         iReader.close();
         directory.close();
-//        IOUtils.rm(indexPath);
+        IOUtils.rm(indexPath);
 
     }
 
